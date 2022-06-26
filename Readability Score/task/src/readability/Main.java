31598e6e3e693ff8f87843082659adb82a14507d
dev-otaher <dev.omar.taher@gmail.com>
2022-06-26 20:07:57 +0300

    Stage 2/4

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,11 +1,19 @@
 package readability;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        System.out.println(text.length() > 100 ? "HARD" : "EASY");
+        String[] sentences = text.split("[.!?]");
+        int wordCount = 0;
+        for (String sentence : sentences) {
+            String[] tokens = sentence.split("\\s");
+            wordCount += tokens.length;
+        }
+        int avg = wordCount / sentences.length;
+        System.out.println(avg > 10 ? "HARD" : "EASY");
     }
 }
