4a04aa0a7a4c7b894d431f554b30a4ef553e1559
dev-otaher <dev.omar.taher@gmail.com>
2022-06-27 23:39:49 +0300

    Stage 3/4

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,19 +1,43 @@
 package readability;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        String[] sentences = text.split("[.!?]");
-        int wordCount = 0;
-        for (String sentence : sentences) {
-            String[] tokens = sentence.split("\\s");
-            wordCount += tokens.length;
+    public static String parseData(String filename) {
+        File file = new File(filename);
+        try (Scanner scanner = new Scanner(file)) {
+            String text = "";
+            while (scanner.hasNext()) {
+                text += scanner.nextLine();
+            }
+            return text;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
-        int avg = wordCount / sentences.length;
-        System.out.println(avg > 10 ? "HARD" : "EASY");
+    }
+
+    public static String getAgeByScore(double score) {
+        double minAge = 5 + Math.ceil(score);
+        double maxAge = minAge + 1;
+        if (Math.ceil(score) == 13) {
+            maxAge = 24;
+        }
+        return String.format("%.0f-%.0f", minAge, maxAge);
+    }
+
+    public static void main(String[] args) {
+        String text = parseData(args[0]);
+        System.out.println("The text is:\n" + text);
+        double words = text.split("\\s").length;
+        System.out.printf("Words: %.0f\n", words);
+        double sentences = text.split("[.!?]").length;
+        System.out.printf("Sentences: %.0f\n", sentences);
+        double characters = text.replaceAll("\\s", "").split("").length;
+        System.out.printf("Characters: %.0f\n", characters);
+        double score = (4.71 * (characters / words)) + (0.5 * (words / sentences)) - 21.43;
+        System.out.printf("The score is: %.2f\n", score);
+        System.out.printf("This text should be understood by %s-year-olds.", getAgeByScore(score));
     }
 }
